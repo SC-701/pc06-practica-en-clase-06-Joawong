0b0b56af5b7f4eb56e8de2bd382d19d786b541b6
Joawong <49043721+Joawong@users.noreply.github.com>
2026-03-02 17:15:48 -0600

    Copiando el proyecto del repo anterior
    Copiando el proyecto del repo anterior

---
+++ Vehiculos/Servicios/RevisionServicio.cs
@@ -0,0 +1,36 @@
+﻿using Abstracciones.Interfaces.Reglas;
+using Abstracciones.Interfaces.Servicios;
+using Abstracciones.Modelos.Servicios.Revision;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Servicios
+{
+    public class RevisionServicio : IRevisionServicio
+    {
+        private readonly IConfiguracion _configuracion;
+        private readonly IHttpClientFactory _httpClient;
+
+        public RevisionServicio(IConfiguracion configuracion, IHttpClientFactory httpClient)
+        {
+            _configuracion = configuracion;
+            _httpClient = httpClient;
+        }
+
+        public async Task<Revision> Obtener(string placa)
+        {
+            var endpoint = _configuracion.ObtenerMetodo("ApiEndPointsRevision", "ObtenerRevision");
+            var servicioRegistro = _httpClient.CreateClient("ServicioRevision");
+            var respuesta = await servicioRegistro.GetAsync(string.Format(endpoint, placa));
+            respuesta.EnsureSuccessStatusCode();
+            var resultado = await respuesta.Content.ReadAsStringAsync();
+            var opciones = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
+            var resultadoDeserializado = JsonSerializer.Deserialize<List<Revision>>(resultado, opciones);
+            return resultadoDeserializado.FirstOrDefault();
+        }
+    }
+}
